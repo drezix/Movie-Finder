bdd8fff7995aa85ee6ab816bac72e763bf084022
drezix <andre.t.agostinis@gmail.com>
2025-02-07 07:15:18 -0300

    refactor(db): refatorar função para conectar no banco de dados

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const connectDB = require('./src/config/db');
+require('./src/config/db');
 
 const app = express()
 const port = 3000
+
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-require('./src/config/db');
 module.exports = app;
--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,19 +3,18 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-try{
-  connect = () => {
-    mongoose.connect(`mongodb://localhost:27017/moviefinder`);
-    console.log('Connected to the database');
+const connectDB = async () => {
+  try{
+    await mongoose.connect(process.env.MONGO_URI);
+      console.log('Connected to the database');
+  } catch (error) {
+    console.log('Error connecting to the database', error);
+    process.exit(1);
   }
-} catch (error) {
-  console.log('Error connecting to the database', error);
-}
-  
+
   mongoose.connection.on('error', err => {
     logError(err);
   });
+};
 
-connect();
-
-module.exports = mongoose;
+module.exports = connectDB;
