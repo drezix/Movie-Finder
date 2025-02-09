872e48813fadf66f59b0bad37348c085aaee2154
drezix <andre.t.agostinis@gmail.com>
2025-02-09 16:28:30 -0300

    feat(middleware): implementar middleware de authenticação com token

--- backend/src/routes/movieRoutes.js
+++ backend/src/routes/movieRoutes.js
@@ -1,6 +1,9 @@
+const requireAuth= require('../middlewares/authMiddleware');
 const express = require('express');
 const movieController = require('../controllers/movieController');
 const router = express.Router();
+
+router.use(requireAuth);
 
 // POST /movies
 router.post('/insert', movieController.insert);
