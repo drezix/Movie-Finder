bb4ed470d051db8d96221f77ec03342e45733b51
drezix <andre.t.agostinis@gmail.com>
2025-02-10 10:58:35 -0300

    feat(https): implementar HTTPS no servidor com certificado SSL

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,25 @@
-const express = require('express');
-const cors = require('cors'); 
 const connectDB = require('./src/config/db');
 const authRoutes = require('./src/routes/authRoutes');
 const movieRoutes = require('./src/routes/movieRoutes');
 require('dotenv').config({ path: './backend/.env' });
 
+const fs = require('fs');
+const privateKey = fs.readFileSync('C:/code/FullStack/moviefinder.local-key.pem', 'utf8');
+const certificate = fs.readFileSync('C:/code/FullStack/moviefinder.local.pem', 'utf8');
+
+const credentials = {key: privateKey, cert: certificate};
+
+const express = require('express');
 const app = express();
 
 connectDB();
 
+const cors = require('cors'); 
 app.use(cors());  
 
 
 app.use(cors({
-  origin: 'http://localhost:3000', 
+  origin: 'https://moviefinder.local:8443', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
@@ -24,5 +30,21 @@ app.use(express.json());
 //--------------------------------------------AUTH-----------------------------------------------
 app.use('/auth', authRoutes);
 app.use('/movies', movieRoutes);
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
+
+const http = require('http');
+const https = require('https');
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(8080);
+httpsServer.listen(8443);
 
 module.exports = app;
