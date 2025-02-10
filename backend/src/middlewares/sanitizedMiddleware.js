4e00bff5adcec9bc9342e13ffc800bfab3afa77d
drezix <andre.t.agostinis@gmail.com>
2025-02-10 12:46:31 -0300

    feat(xss): implementar biblioteca xss também em sanitizeMiddleware

--- backend/src/middlewares/sanitizedMiddleware.js
+++ backend/src/middlewares/sanitizedMiddleware.js
@@ -1,4 +1,5 @@
 const sanitizer = require('perfect-express-sanitizer');
+const xss = require('xss');
 
 const sanitizedMiddleware = (req, res, next) => {
   sanitizer.clean({
@@ -6,6 +7,15 @@ const sanitizedMiddleware = (req, res, next) => {
     noSql: true,
     sql: true,
   })(req, res, next);
+
+  if (req.body) {
+    req.body = Object.keys(req.body).reduce((acc, key) => {
+      acc[key] = xss(req.body[key]);
+      return acc;
+    }, {});
+  }
+
+  next();
 };
 
 module.exports = sanitizedMiddleware;
